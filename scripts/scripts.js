cf39b91c0e3fc2eeb3376f5d7c5f446b74f55c40
hugostanley <stanleyhugo06@gmail.com>
2022-06-08 13:55:35 +0800

    Upload initial file structure and Scripts test

---
+++ scripts/scripts.js
@@ -0,0 +1,33 @@
+const first = document.querySelector('.one')
+const two = document.querySelector('.two')
+const three = document.querySelector('.three')
+const four = document.querySelector('.four')
+const five = document.querySelector('.five')
+const six = document.querySelector('.six')
+const seven = document.querySelector('.seven')
+const eight = document.querySelector('.eight')
+const nine = document.querySelector('.nine')
+console.log(first)
+
+
+
+let boardState = [
+    ['','',''],
+    ['','',''],
+    ['','','']
+]
+
+let hello = 'hey';
+boardState[0][2] = hello
+
+first.textContent = boardState[0][0]
+two.textContent = boardState[0][1]
+three.textContent = boardState[0][2]
+
+four.textContent = boardState[1][0]
+five.textContent = boardState[1][1]
+six.textContent = boardState[1][2]
+
+seven.textContent = boardState[2][0]
+eight.textContent = boardState[2][1]
+nine.textContent = boardState[2][2]
